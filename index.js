94a1d8d488ef7ac0e16e282a053592024ce53e77
Luay Bseiso <luay@buttercloud.com>
2016-02-18 10:52:51 +0200

    add support for long text + rename private functions with underscore

--- index.js
+++ index.js
@@ -1,29 +1,58 @@
 var _ = require('lodash');
 
 var FormFiller = {
-  checkItems: function(elementIds) {
-    elementIds.forEach(function(id, index) {
+  _fillInput: function(id, text, isLongText) {
+    var lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    var el = document.getElementById(id);
+    var value = isLongText ? lorem : (text || "This is text.");
+    el.value = value;
+  },
+
+  _checkItems: function(elementIds) {
+    elementIds.forEach(function(id) {
       var el = document.getElementById(id);
       el.checked = true;
     });
   },
   
-  selectOption: function(selectTagIds, indexToSelect) {
-    selectTagIds.forEach(function(id, index) {
+  _selectOption: function(selectTagIds, index) {
+    selectTagIds.forEach(function(id) {
       var el = document.getElementById(id);
-      el[indexToSelect].selected = true;
+      el[index].selected = true;
     });
   },
-  
-  fill: function(map) {
+
+  _fillTextFields: function(inputs) {
+    var self = this;
+
+    inputs.forEach(function(input, i) {
+      if (_.isPlainObject(input)) {
+        if (_.has(input, 'id')) {
+          self._fillInput(input.id, input.text, input.long || false);
+        };
+      } else {
+        self._fillInput(input);
+      };
+    });
+  },
+
+  _fill: function(map) {
     if (_.isArray(map.check)) {
-      this.checkItems(map.check);
+      this._checkItems(map.check);
     };
     
     if (!_.isEmpty(map.select)) {
       if (_.isArray(map.select.items)) {
-        this.selectOption(map.select.items, map.select.index || 0);
-      }
+        this._selectOption(map.select.items, map.select.index || map.select.items.length - 1);
+      };
+    };
+    
+    if (_.isArray(map.text)) {
+      this._fillTextFields(map.text);
+    };
+    
+    if (_.isArray(map.radio)) {
+      this._checkItems(map.radio)
     };
   },
   
@@ -33,7 +62,7 @@ var FormFiller = {
     var button = document.getElementsByClassName(el)[0];
 
     button.addEventListener('click', function() {
-      self.fill(map);
+      self._fill(map);
     });    
   }
 };
