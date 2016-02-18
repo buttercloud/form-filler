4016c73d463abb0c0d16fbb075c5cdba0c1c1c95
Luay Bseiso <luay@buttercloud.com>
2016-02-18 11:24:05 +0200

    add support to fill all the form given an ID + refactored functions that fill, select, and check elements

--- index.js
+++ index.js
@@ -1,69 +1,113 @@
 var _ = require('lodash');
 
 var FormFiller = {
-  _fillInput: function(id, text, isLongText) {
+  _fillTextField: function(el, text, isLongText) {
     var lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    var el = document.getElementById(id);
     var value = isLongText ? lorem : (text || "This is text.");
     el.value = value;
   },
 
-  _checkItems: function(elementIds) {
+  _checkItem: function(el) {
+    el.checked = true;
+  },
+
+  _selectOption: function(el, index) {
+    el[index].selected = true;
+  },
+
+  _checkItemsWithIds: function(elementIds) {
+    var self = this;
+
     elementIds.forEach(function(id) {
       var el = document.getElementById(id);
-      el.checked = true;
-    });
-  },
-  
-  _selectOption: function(selectTagIds, index) {
-    selectTagIds.forEach(function(id) {
-      var el = document.getElementById(id);
-      el[index].selected = true;
+      self._checkItem(el);
     });
   },
 
-  _fillTextFields: function(inputs) {
+  _selectOptionsWithIds: function(selectTagIds, index) {
+    var self = this;
+
+    selectTagIds.forEach(function(id) {
+      var el = document.getElementById(id);
+      self._selectOption(el, index);
+    });
+  },
+
+  _fillTextFieldsWithIds: function(inputs) {
     var self = this;
 
     inputs.forEach(function(input, i) {
       if (_.isPlainObject(input)) {
         if (_.has(input, 'id')) {
-          self._fillInput(input.id, input.text, input.long || false);
+          var el = document.getElementById(input.id);
+          self._fillTextField(el, input.text, input.long || false);
         };
       } else {
-        self._fillInput(input);
+        var el = document.getElementById(input);
+        self._fillTextField(el);
       };
     });
   },
 
   _fill: function(map) {
     if (_.isArray(map.check)) {
-      this._checkItems(map.check);
+      this._checkItemsWithIds(map.check);
     };
     
     if (!_.isEmpty(map.select)) {
       if (_.isArray(map.select.items)) {
-        this._selectOption(map.select.items, map.select.index || map.select.items.length - 1);
+        this._selectOptionsWithIds(map.select.items, map.select.index || map.select.items.length - 1);
       };
     };
     
     if (_.isArray(map.text)) {
-      this._fillTextFields(map.text);
+      this._fillTextFieldsWithIds(map.text);
     };
     
     if (_.isArray(map.radio)) {
-      this._checkItems(map.radio)
+      this._checkItemsWithIds(map.radio)
     };
   },
   
-  init: function(map, className) {
+  _fillAllForm: function(formId) {
+    var self = this;
+    var formEl = document.getElementById(formId);
+    var children = formEl.children;
+
+    for (var i = 0; i < children.length; i++) {
+      var el = children[i];
+
+      switch (el.nodeName) {
+        case "INPUT":
+          el.type === "text" ? self._fillTextField(el, null, false) : self._checkItem(el);
+          break;
+        case "SELECT":
+          self._selectOption(el, el.length - 1);
+          break;
+        case "TEXTAREA":
+          self._fillTextField(el, null, true);
+          break;
+        default:
+      }
+    }
+  },
+
+  _bindAndInit: function(className, fillFn) {
     var self = this;
     var el = className || 'form-filler';
     var button = document.getElementsByClassName(el)[0];
 
     button.addEventListener('click', function() {
-      self._fill(map);
+      fillFn();
     });    
+  },
+
+  init: function(map, className) {
+    this._bindAndInit(className, this._fill.bind(this, map));
+  },
+
+  fillAll: function(formId, className) {
+    this._bindAndInit(className, this._fillAllForm.bind(this, formId));
   }
 };
 
