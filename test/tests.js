43de8388a1a4ff71d2b6a01a9881408c847530e6
Luay Bseiso <luay@buttercloud.com>
2016-02-21 15:45:10 +0200

    add test environment with empty tests

---
+++ test/tests.js
@@ -0,0 +1,61 @@
+var chai = require('chai');
+var expect = chai.expect();
+var fs = require('fs');
+var FormFiller = require('../index.js');
+
+describe("Classes", function() {
+  describe("FormFiller", function() {
+    describe("#fillAll", function() {
+      it("should fill out the whole form", function() {
+      });
+    });
+
+    describe("#fill", function() {
+      it("should fill out the provided elements", function() {
+      });
+    });
+  });
+
+  describe("FormFillerUtil", function() {
+    describe("#_fillTextField", function() {
+      it("", function() {});
+    });
+
+    describe("#_checkItem", function() {
+      it("", function() {});
+    });
+
+    describe("#_selectOption", function() {
+      it("", function() {});
+    });
+
+    describe("#_selectOptionWithValue", function() {
+      it("", function() {});
+    });
+
+    describe("#_checkItemsWithIds", function() {
+      it("", function() {});
+    });
+
+    describe("#_selectOptionsWithIds", function() {
+      it("", function() {});
+    });
+
+    describe("#_fillTextFieldsWithIds", function() {
+      it("", function() {});
+    });
+
+    describe("#_fill", function() {
+      it("", function() {});
+    });
+
+    describe("#_fillAllForm", function() {
+      it("it should fill out the whole form", function() {
+      });
+    });
+
+    describe("#_bindAndInit", function() {
+      it("", function() {});
+    });
+  });
+});
